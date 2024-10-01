e338c71ecd7e346ed89d71f951ad6279767b153d
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2024-10-01 17:32:55 -0300

    actualizacion de title a name

--- src/components/ItemCarrito.jsx
+++ src/components/ItemCarrito.jsx
@@ -3,14 +3,14 @@ import '../components/ItemCarrito.css'
 
 
 export default function ItemCarrito(props) {
-    const { id, title, eliminarCarrito, price, image_url, cantidad, incrementarCantidad } = props
+    const { id, name, eliminarCarrito, price, image_url, cantidad, incrementarCantidad } = props
     console.log(image_url, "url de la imagen que llega a itemcarrito")
     return (
         <>
             <div className="wrapper">
-                <img src={image_url} alt={title} width={100} height={100} />
+                <img src={image_url} alt={name} width={100} height={100} />
                 <div>
-                    <h1>Nombre: {title}</h1>
+                    <h1>Nombre: {name}</h1>
                 </div>
                 <div><h1>Precio:{price}</h1></div>
                 <div><h1>Cantidad: {cantidad}</h1></div>
